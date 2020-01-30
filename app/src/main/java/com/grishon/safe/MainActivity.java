9137b44042e464ae5c5e24a6339a4715d7d01cd6
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-01-30 20:22:20 +0300

    Add button to screen

--- app/src/main/java/com/grishon/safe/MainActivity.java
+++ app/src/main/java/com/grishon/safe/MainActivity.java
@@ -3,12 +3,32 @@ package com.grishon.safe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button clickMe;
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        clickMe = findViewById(R.id.button);
+        name = findViewById(R.id.textView);
+
+        clickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name.setText("You have clicked me.");
+            }
+        });
     }
 }
+K
